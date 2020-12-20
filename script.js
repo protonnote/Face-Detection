2527bf0c0ec048b40b36d1a0f8a7723861856f15
protonnote <nongnumainaka@gmail.com>
2020-12-21 02:21:19 +0700

    ..

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const video = document.getElementById('video')
-
+const video = document.getElementById('web-video')
+/*
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
-
+*/
 function startVideo() {
-  navigator.mediaDevices.getUserMedia(
+  navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
 }
-
+/*
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
@@ -28,4 +28,6 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
-})
+})*/
+
+startVideo()
